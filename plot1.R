1cf72f0e3f28a15d5d51a567a7e1cf6022873b6f
Bernardo Gomes <BernardoGomes@users.noreply.github.com>
2015-06-05 23:48:59 -0300

    Create plot1.R
    Script to create the plot1.png.

---
+++ plot1.R
@@ -0,0 +1,30 @@
+#project #01 -  Exploratory Data Analysis
+
+library(dplyr)
+
+#this script assumes that your working directory 
+#contains the file already unzipped. 
+#uncomment the following line to get to the right directory
+setwd("C:/Users/Bernardo/Documents/Coursera/04 - Exploratory Data Analysis/Code")
+
+file <- "household_power_consumption.txt"
+
+data <- read.csv(file, header = TRUE, sep=";", na.strings = "?", stringsAsFactors = FALSE)
+
+nrow(data) #should be 2.075.259
+ncol(data) #should be 9
+
+fdata <- filter(data, Date =='1/2/2007' | Date == '2/2/2007')
+
+fdata$NewDate <- strptime(paste(fdata$Date, fdata$Time), "%d/%m/%Y %T")
+
+#fdata$Date <- strptime(fdata$Date, "%d/%m/%Y")
+
+summary(fdata)
+windows()
+hist(fdata$Global_active_power, 
+     col="red", 
+     main="Global Active Power", 
+     xlab = "Global Active Power (kilowatts)") 
+dev.copy(png, file = "plot1.png", width=480, height=480)
+dev.off()
